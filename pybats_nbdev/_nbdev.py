94a47d4fb79321ae47527e47eb0bf32a41427743
lavinei <lavine.isaac@gmail.com>
2020-07-15 18:40:22 -0400

    adding in the pybats library

--- pybats_nbdev/_nbdev.py
+++ pybats_nbdev/_nbdev.py
@@ -2,9 +2,32 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"dglm": "00_dglm.ipynb"}
+index = {"dglm": "00_dglm.ipynb",
+         "bern_dglm": "00_dglm.ipynb",
+         "pois_dglm": "00_dglm.ipynb",
+         "update_F": "01_update.ipynb",
+         "update": "01_update.ipynb",
+         "update_dlm": "01_update.ipynb",
+         "update_bindglm": "01_update.ipynb",
+         "forecast_aR": "02_forecast.ipynb",
+         "forecast_R_cov": "02_forecast.ipynb",
+         "forecast_marginal": "02_forecast.ipynb",
+         "forecast_path": "02_forecast.ipynb",
+         "forecast_path_copula": "02_forecast.ipynb",
+         "forecast_path_dlm": "02_forecast.ipynb",
+         "forecast_marginal_bindglm": "02_forecast.ipynb",
+         "forecast_path_copula_sim": "02_forecast.ipynb",
+         "forecast_path_copula_density_MC": "02_forecast.ipynb",
+         "forecast_joint_copula_sim": "02_forecast.ipynb",
+         "forecast_joint_copula_density_MC": "02_forecast.ipynb",
+         "multivariate_t": "02_forecast.ipynb",
+         "multivariate_t_density": "02_forecast.ipynb",
+         "forecast_state_mean_and_var": "02_forecast.ipynb",
+         "forecast_marginal_density_MC": "02_forecast.ipynb"}
 
-modules = ["dglm.py"]
+modules = ["dglm.py",
+           "update.py",
+           "forecast.py"]
 
 doc_url = "https://lavinei.github.io/pybats_nbdev/"
 
